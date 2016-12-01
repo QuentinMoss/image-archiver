5e6c7c2f1ce74957cea253cde4f569c4a204aaef
Quentin Moss <quejmoss@gmail.com>
2016-11-30 16:31:56 -0800

    fix syntax

--- image-archiver.py
+++ image-archiver.py
@@ -1,27 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os,sys
+import os,sys,re
 
 def main():
     # Take argument on script execution. Provided directory should contain all images. Referred to as parent directory
     directory_path = sys.argv[1]
+    archiev_path   = sys.argv[2]
 
     # Create the archiev directory if it does not exist
     createArchiveDirectory()
 
     # Print out the directory structure we will be working with
     for folderName, subfolders, filenames in os.walk(directory_path):
-        print('The provided parent directory is *' + folderName + '*')
-    for subfolder in subfolders:
-        print('The parent directory contains the following sub folders *' + folderName + ': ' + subfolder + '*')
-    for filename in filenames:
-        print('Found the following files *' + folderName + '/'+ filename + '*')
+        print('The provided parent directory is *' + directory_path + '*')
+        for subfolder in subfolders:
+            print('The parent directory contains the following sub folders *' + folderName + ': ' + subfolder + '*')
+        for filename in filenames:
+            print('Found the following files *' + folderName + '/'+ filename + '*')
 
 # Define function that creates directory if it does not exist
 def createArchiveDirectory():
     if not os.path.exists('archive'):
         os.makedirs('archive')
+
 
 if __name__ == "__main__":
     main()
