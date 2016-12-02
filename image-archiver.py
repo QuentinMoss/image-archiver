9bec57dc3349a3b4f72fd52b297de2e49a2690a5
Quentin Moss <quejmoss@gmail.com>
2016-12-02 13:41:50 -0800

    Add dir structure creation

--- image-archiver.py
+++ image-archiver.py
@@ -15,18 +15,24 @@ def main():
 
     # Create the archiev directory if it does not exist
     createArchiveDirectory()
-
+    # Create directory structure if *doesn't exist* before we begin sync tasks.
+    createDirStructure(source_path, archive_path)
     # Create list of files that we will be archiving
     files_for_sync = filecmp.dircmp(source_path, archive_path).left_only
-    print files_for_sync
     for files in files_for_sync:
         shutil.copy(source_path + "/" + files, archive_path)
-
 
 # Creates archiev directory if it does not exist
 def createArchiveDirectory():
     if not os.path.exists('archive'):
         os.makedirs('archive')
+
+def createDirStructure(s, a):
+    for root, subdirs, files in os.walk(s):
+        for subdir in subdirs:
+            dir_create = os.path.join(a, subdir)
+            if not os.path.exists(dir_create):
+                os.makedirs(dir_create)
 
 if __name__ == "__main__":
     main()
