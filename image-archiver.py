7c5e3204fcd76d6fa4834569845994495090e445
Quentin Moss <quejmoss@gmail.com>
2016-12-01 16:37:41 -0800

    Add file diff + copy

--- image-archiver.py
+++ image-archiver.py
@@ -2,24 +2,31 @@
 # -*- coding: utf-8 -*-
 
 import os,sys,re
+import filecmp
+import shutil
+
+# Take argument on script execution. Provided directory should contain all images. Referred to as parent directory
+source_path = sys.argv[1]
+archive_path   = sys.argv[2]
 
 def main():
-    # Take argument on script execution. Provided directory should contain all images. Referred to as parent directory
-    directory_path = sys.argv[1]
-    archiev_path   = sys.argv[2]
+    # Print out the directory structure we will be working with
+    print('The provided source directory is *' + source_path + '*')
 
     # Create the archiev directory if it does not exist
     createArchiveDirectory()
 
-    # Print out the directory structure we will be working with
-    for folderName, subfolders, filenames in os.walk(directory_path):
-        print('The provided parent directory is *' + directory_path + '*')
+    # Create list of files that we will be archiving
+    files_for_sync = filecmp.dircmp(source_path, archive_path).left_only
+    print files_for_sync
+    for files in files_for_sync:
+        shutil.copy(source_path + "/" + files, archive_path)
+
 
 # Creates archiev directory if it does not exist
 def createArchiveDirectory():
     if not os.path.exists('archive'):
         os.makedirs('archive')
-
 
 if __name__ == "__main__":
     main()
